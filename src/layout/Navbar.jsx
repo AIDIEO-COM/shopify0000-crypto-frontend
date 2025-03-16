8a1b58bab398e618e2cf7f52b4731174df35ea7b
razuahmedjoy <rjjoybzs420@gmail.com>
2025-03-17 01:44:16 +0600

    login redirected

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -6,19 +6,7 @@ const Navbar = () => {
   const navigate = useNavigate();
 
   const handleLogout = () => {
-    Swal.fire({
-      title: "Are you sure?",
-      text: "You will be logged out!",
-      icon: "warning",
-      showCancelButton: true,
-      confirmButtonColor: "#d33",
-      cancelButtonColor: "#3085d6",
-      confirmButtonText: "Logout",
-    }).then((result) => {
-      if (result.isConfirmed) {
-        navigate("/"); // Redirect to home after confirmation
-      }
-    });
+    navigate("/");
   };
 
   return (
